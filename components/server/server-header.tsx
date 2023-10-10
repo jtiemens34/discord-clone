25baa6b15d3df8f3690a6170e7a8d50b078a3f3a
elegantlytragic <zomg.its.jesse@hotmail.com>
2023-10-09 17:50:37 -0700

    Edit Server Modal finished

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -47,6 +47,7 @@ const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("editServer", { server })}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Server Settings
